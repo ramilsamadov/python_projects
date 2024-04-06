3403b2c726eba70c7e32b5c1b55ac7784b17cfbd
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-06 14:30:30 +0200

    Update Text Wrap.py

--- HackerRank/Strings/Text Wrap.py
+++ HackerRank/Strings/Text Wrap.py
@@ -5,3 +5,14 @@ Created on Fri Apr  5 23:46:04 2024
 @author: Samadov
 """
 
+import textwrap
+
+def wrap(string, max_width):
+    wrapped=textwrap.wrap(string,max_width)
+        
+    return "\n".join(wrapped)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
