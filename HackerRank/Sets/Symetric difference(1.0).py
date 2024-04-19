589f368f0f80c158f5f4d75776dc52d4393371ee
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-19 15:33:02 +0200

    Update Symetric difference(1.0).py

--- HackerRank/Sets/Symetric difference(1.0).py
+++ HackerRank/Sets/Symetric difference(1.0).py
@@ -4,4 +4,11 @@ Created on Tue Apr 16 22:29:22 2024
 
 @author: Samadov
 """
-s
+m,a=int(input()),input().split()
+n,b=int(input()),input().split()
+a_set=set(a)
+b_set=set(b)
+a_dif=a_set.difference(b_set)
+b_dif=b_set.difference(a_set)
+all_union=a_dif.union(b_dif)
+print("\n".join(sorted(all_union,key=int)))
