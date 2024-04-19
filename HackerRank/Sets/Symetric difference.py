1864f32ffded446be0c7fb5d430e3a230b27e256
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-19 15:28:47 +0200

    Update Symetric difference.py

--- HackerRank/Sets/Symetric difference.py
+++ HackerRank/Sets/Symetric difference.py
@@ -5,3 +5,10 @@ Created on Tue Apr 16 22:29:22 2024
 @author: Samadov
 """
 
+m=int(input())
+a=set(map(int,input().split()))
+n=int(input())
+b=set(map(int,input().split()))
+sortedd=sorted((a-b).union(b-a))
+for i in sortedd:
+    print(i)
