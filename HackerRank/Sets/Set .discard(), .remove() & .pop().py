e0d763d8d711c116ccc4487fce3f2b5c6ce81c92
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-20 23:35:31 +0200

    Add files via upload

---
+++ HackerRank/Sets/Set .discard(), .remove() & .pop().py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 23:34:40 2024
+
+@author: Samadov
+"""
+
+number_of_elements = int(input())
+elements = set(map(int, input().split()))
+number_of_commands = int(input())
+for _ in range(number_of_commands):
+    cmd = list(input().split())
+    if len(cmd) == 1:
+        elements.pop()
+    else:
+        value = int(cmd[1])
+        operation = cmd[0]
+        if operation == "discard":
+            elements.discard(value)
+        else:
+            elements.remove(value)
+print(sum(elements))
+
