3d4223870047ed22516bb1fa2083a712a0e7995b
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-25 01:05:06 +0200

    Add files via upload

---
+++ HackerRank/Math/Power - Mod Power.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 01:04:19 2024
+
+@author: Samadov
+"""
+
+a,b,m=int(input()),int(input()),int(input())
+poww=pow(a,b)
+divmodd=pow(a,b,m)
+print(f"{poww}\n{divmodd}")
