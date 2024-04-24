9f5aea3403114b8518db18b57b3e43d86d11dd65
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-25 00:57:52 +0200

    Add files via upload

---
+++ HackerRank/Math/Mod Divmod (Fast).py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 00:53:36 2024
+
+@author: Samadov
+"""
+
+num1= int(input())
+num2= int(input())
+sum=(divmod(num1, num2))
+print(f"{sum[0]}\n{sum[1]}\n{sum}")
