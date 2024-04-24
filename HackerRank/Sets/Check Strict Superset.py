726ca06c1a25923f4a2d148bfec73d5e7e3b5b3f
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-24 16:21:11 +0200

    Add files via upload

---
+++ HackerRank/Sets/Check Strict Superset.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 16:20:13 2024
+
+@author: Samadov
+"""
+
+def checker():
+    subset=set(map(int,input().split()))
+    if subset.issuperset(a):
+        if len(a)==len(subset):
+            listt.append(0)
+        else:
+            listt.append(1)
+    else:
+        listt.append(0)
+        
+    
+    
+    
+    
+a=set(map(int,input().split()))
+a1=int(input())
+listt=[]
+for i in range(int(input())):
+    checker()
+    
+if all(listt)==1:
+    print(True)
+else:
+    print(False)
