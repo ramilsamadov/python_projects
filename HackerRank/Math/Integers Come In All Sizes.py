67e2233c1f6fe1c56f3cc086b2786af9cf81ac4b
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-04-25 01:13:13 +0200

    Add files via upload

---
+++ HackerRank/Math/Integers Come In All Sizes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 01:11:59 2024
+
+@author: Samadov
+"""
+
+
+a,b,c,d=int(input()),int(input()),int(input()),int(input())
+print(pow(a,b)+pow(c,d))
