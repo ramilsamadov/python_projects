a61dd05ad7f13cc90f8fb8a0f754ab42b3265972
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-05-03 17:21:37 +0200

    Update Maximize It!.py

--- HackerRank/Itertools/Maximize It!.py
+++ HackerRank/Itertools/Maximize It!.py
@@ -5,3 +5,20 @@ Created on Thu May  2 23:20:50 2024
 @author: Samadov
 """
 
+from itertools import product
+
+k,m=list(map(int,input().split()))
+listt=[]
+for i in range(k):
+    listt.append(list(map(int,input().split()))[1:])
+    
+list_product=list(product(*listt))
+
+final=[]
+for j in list_product:
+    total=0
+    for k in j:
+        total=total+k**2
+    final.append(total%m)
+print(max(final))
+    
