3710c4a6fa3d5922dacc8aed94ba4181e2d8f637
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-05-07 01:52:17 +0200

    Add files via upload

---
+++ HackerRank/Classes/str rep of an obj without __str__().py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 01:49:48 2024
+
+@author: 48513
+"""
+
+class MyClass():
+    def __init__(a,b,c):
+        a.name=b
+        a.surname=c
+y=MyClass("Ramil", "Samadov")
+print(y)        
