a39063837b4af1c5e5b4ff7ea26c5d677518ff5b
Ramil Samadov <90143263+ramilsamadov@users.noreply.github.com>
2024-05-07 02:26:07 +0200

    Add files via upload

---
+++ HackerRank/Classes/Pass Statement.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 02:25:20 2024
+
+@author: Samadov
+"""
+
+class Person:
+  pass
+
+# having an empty class definition like this, would raise an error without the pass statement
